b8fbdabe723a9c2c57622d7e065c611492917e8e
sean lewis <seanmartinlewis@gmail.com>
2016-11-11 11:12:12 -0600

    added Game component

---
+++ components/Game.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Slider,
+  PixelRatio,
+  TouchableHighlight
+} from 'react-native';
+
+export default class Game extends Component {
+  constructor(props){
+    super(props)
+  }
+  
+    render(){
+      return(
+
+        <Text>Create game Component</Text>
+
+      )
+    }
+}
